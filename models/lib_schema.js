8e54e2c10436d95b5a18756d9c16f4b3285585a5
KimBitrus26 <bitruschoji4real@gmail.com>
2022-09-22 16:26:32 +0100

    First version of application moved into GitHub

---
+++ models/lib_schema.js
@@ -0,0 +1,28 @@
+// // Import the mongoose module
+// const mongoose = require("mongoose");
+
+// // Set up default mongoose connection
+// const mongoDB = "mongodb://127.0.0.1/my_database";
+// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// // Get the default connection
+// const db = mongoose.connection;
+
+// // Bind connection to error event (to get notification of connection errors)
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// mongodb+srv://mydatabase1:dbpassword26@cluster0.xmzqq5e.mongodb.net/local_library?retryWrites=true&w=majority
+
+// // Require Mongoose
+// const mongoose = require("mongoose");
+
+// // Define a schema
+// const Schema = mongoose.Schema;
+
+// const SomeModelSchema = new Schema({
+//   a_string: String,
+//   a_date: Date,
+// });
+
+// // Export function to create "SomeModel" model class
+// module.exports = mongoose.model("SomeModel", SomeModelSchema);
